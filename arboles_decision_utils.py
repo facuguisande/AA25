edabde134082699d08ad4714621bbd124df48970
Facundo <facundoguisande16@gmail.com>
2025-09-21 22:24:25 -0300

    Se genera nuevo grafico comparativo, y se corrigen nombres de funcinoes

--- arboles_decision_utils.py
+++ arboles_decision_utils.py
@@ -12,8 +12,8 @@ def entrenar_y_evaluar_arbol(X_train, y_train, X_test, y_test, min_gain_info=0.0
 
     # Calcular métricas de evaluación
     acc = accuracy_score(y_test, y_pred)
-    prec = precision_score(y_test, y_pred, average='macro')
-    rec = recall_score(y_test, y_pred, average='macro')
-    f1 = f1_score(y_test, y_pred, average='macro')
+    prec = precision_score(y_test, y_pred, average='macro', zero_division=0)
+    rec = recall_score(y_test, y_pred, average='macro', zero_division=0)
+    f1 = f1_score(y_test, y_pred, average='macro', zero_division=0)
     cm = confusion_matrix(y_test, y_pred)
     return acc, prec, rec, f1, cm, y_pred
